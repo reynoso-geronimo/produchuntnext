cd9c632b9a6944c34ed9970510b89d324b49e132
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-08-26 16:50:31 -0300

    CSS a la Barra

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,53 +1,78 @@
 import React from "react";
-import Buscar from "../ui/Buscar";
-import Navegacion from "./Navegacion";
 import Link from "next/link";
 import styled from "@emotion/styled";
-import {jsx, css } from "@emotion/react";
+import { jsx, css } from "@emotion/react";
 
-const ContenedorHeader= styled.div`
+import Buscar from "../ui/Buscar";
+import Navegacion from "./Navegacion";
+import Boton from "../ui/Boton";
+
+const ContenedorHeader = styled.div`
   max-width: 1200px;
   width: 95%;
-  margin:0 auto;
-  @media (min-width:768px){
+  margin: 0 auto;
+  @media (min-width: 768px) {
     display: flex;
     justify-content: space-between;
   }
-  `
+`;
 
-  const Logo = styled.p`
-    color: var(--naranja);
-    font-size: 4rem;
-    line-height: 0;
-    font-weight: 700;
-    font-family: 'Roboto Slab' serif;
-    margin-right: 2rem;
-  `;
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab" serif;
+  margin-right: 2rem;
+`;
 
 const Header = () => {
+  const usuario = true;
+
   return (
     <header
-        css={css`
-            border-bottom: 2px solid var(--gris3);
-            padding: 1rem 0;
-
-        `}
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
     >
       <ContenedorHeader>
         <div>
           <Link href="/" passHref>
             <Logo>P</Logo>
-            </Link>
-          
+          </Link>
+
           <Buscar />
 
           <Navegacion />
         </div>
-        <div>
-          <p>Hola: Overlord</p>
-          <button type="button">Cerrar Sesion</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Crear Cuenta</Link>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          {usuario ? (
+            <>
+              <p
+                css={css`
+                  margin-right: 2rem;
+                `}
+              >
+                Hola: Overlord
+              </p>
+              <Boton bgColor="true">Cerrar Sesion</Boton>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/">
+                <Boton>Crear Cuenta</Boton>
+              </Link>
+            </>
+          )}
         </div>
       </ContenedorHeader>
     </header>
