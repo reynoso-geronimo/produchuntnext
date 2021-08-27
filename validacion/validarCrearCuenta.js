d8045de24c1a6a1b72b19e3cc07c38cd3dddd0eb
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-08-27 12:21:14 -0300

    Las Reglas de Validación

---
+++ validacion/validarCrearCuenta.js
@@ -0,0 +1,19 @@
+export default function validarCrearCuenta(valores) {
+  let errores = {};
+
+  //validar el nombre del usuario
+  if (!valores.nombre) {
+    errores.nombre = "El Nombre es Obligatorio";
+  }
+  //validar el email
+  if (!valores.email) {
+    errores.email = "El Email es Obligatorio";
+  }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(valores.email)){
+    errores.email= "Email no valido"
+  }
+  if (!valores.password) {
+    errores.password = "El Password es Obligatorio";
+  }else if (valores.password.lenght<6){
+      errores.password="El password debe ser de al menos 6 caracteres"
+  }
+}
