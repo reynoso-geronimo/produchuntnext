ae81a01a0eaf733e5e7d5403f76ab2772404d0e3
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-02 18:10:04 -0300

    Update index.js
    Consultando la Base de Datos

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React ,{useEffect,useState,useContext}from 'react';
 import Layout from '../components/layout/Layout'
-import firebase from '../firebase'
+import {FirebaseContext} from '../firebase'
 
 
 
 export default function Home() {
+  const [productos, setProductos]=useState([]);
+
+  
+  const {firebase} = useContext(FirebaseContext)
+  
+  useEffect(() => {
+   const obtenerProductos = ()=>{
+    firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+   }
+   obtenerProductos()
+  }, [])
+
+  function manejarSnapshot(snapshot){
+    const productos= snapshot.docs.map(doc=>{
+      return {
+        id:doc.id,
+        ...doc.data()
+      }
+    })
+    setProductos(productos)
+  }
+
   return (
     <Layout>
       <h1>Inicio</h1>
