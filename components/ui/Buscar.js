3e395fe678384b4c6de7ce7649b13702c8cb7b2d
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-03 16:24:06 -0300

    Agregando un Buscador

--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,41 +1,58 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Router from 'next/router'
+
+const InputText = styled.input`
+  border: 1px solid var(--gris3);
+  padding: 1rem;
+  min-width: 300px;
+`;
+const InputSubmit = styled.button`
+  height: 3rem;
+  width: 3rem;
+  display: block;
+  background-size: 4rem;
+  background-image: url("/static/img/buscar.png");
+  background-repeat: no-repeat;
+  position: absolute;
+  right: 1rem;
+  top: 1px;
+  background-color: white;
+  border: none;
+  text-indent: -9999px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Buscar = () => {
+  const [busqueda, setBusqueda] = useState('');
 
-    const InputText =styled.input`
-        border: 1px solid var(--gris3);
-        padding: 1rem;
-        min-width: 300px;
-    `
-    const InputSubmit=  styled.button`
-        height: 3rem;
-        width: 3rem;
-        display: block;
-        background-size: 4rem;
-        background-image: url('/static/img/buscar.png');
-        background-repeat: no-repeat;
-        position: absolute;
-        right: 1rem;
-        top:1px;
-        background-color: white;
-        border: none;
-        text-indent: -9999px;
-        &:hover{
-            cursor: pointer;
-        }
-    `
-
-    return ( 
+  const buscarProducto = e => {
+    e.preventDefault();
+    if(busqueda.trim()==='')return;
+    Router.push({
+        pathname:'/buscar',
+        query:{ q: busqueda}
+    })
+  };
+  return ( 
     <form
         css={css`
             position: relative;
         `}
+        onSubmit={buscarProducto}
     >
-        <InputText type='text' placeholder="Buscar Productos"/>
-        <InputSubmit type='submit'>Buscar</InputSubmit>
-        </form> );
-}
- 
+        <InputText 
+            type="text" 
+            placeholder="Buscar Productos"
+            onChange={e =>  setBusqueda(e.target.value) }
+        />
+
+        <InputSubmit type="submit">Buscar</InputSubmit>
+    </form>
+ );
+};
+
 export default Buscar;
--- pages/buscar.js
+++ pages/buscar.js
@@ -1,13 +1,47 @@
-import Layout from '../components/layout/Layout'
-import React from 'react';
-
+import React, { useEffect, useState } from 'react'
+import Layout from '../components/layout/Layout';
+import { useRouter } from 'next/router';
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 
 export default function Buscar() {
-  return (
-    <Layout>
-      <h1>Buscar</h1>
+  const router = useRouter();
+  const { query: { q }} = router;
+
+  // Todos los productos
+  const { productos } = useProductos('creado');
+  const [ resultado, setResultado ] = useState([]);
+
+  useEffect(() => {
+      const busqueda = q.toLowerCase();
+      const filtro =  productos.filter(producto => {
+        return (
+          producto.nombre.toLowerCase().includes(busqueda) || 
+          producto.descripcion.toLowerCase().includes(busqueda)
+        )
+      });
+      setResultado(filtro);
       
-    </Layout>
-  );
+  }, [ q, productos ]);
+
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+            <div className="contenedor">
+              <ul className="bg-white">
+                  {resultado.map(producto => (
+                      <DetallesProducto
+                          key={producto.id}
+                          producto={producto}
+                      />
+                  ))}
+              </ul>
+            </div>
+        </div>
+      </Layout>
+    </div>
+  )
 }
